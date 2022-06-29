cbd2e37dc50456503ff92c9e3536ffd790a2f616
Dev-PunChi <dev.punchi.84@gmail.com>
2022-06-29 15:43:51 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,27 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
-import Button from "@mui/material/Button";
 import PrimarySearchAppBar from "./PrimarySearchAppBar";
 
 function App() {
-  const [userName, setUserName] = useState("");
-  const [score, setScore] = useState(0);
+  const { unityProvider, sendMessage, addEventListener, removeEventListener } =
+    useUnityContext({
+      loaderUrl: "Build/Build_WebGL.loader.js",
+      dataUrl: "Build/Build_WebGL.data",
+      frameworkUrl: "Build/Build_WebGL.framework.js",
+      codeUrl: "Build/Build_WebGL.wasm",
+    });
 
-  const {
-    unityProvider,
-    sendMessage,
-    addEventListener,
-    removeEventListener,
-    requestFullscreen,
-  } = useUnityContext({
-    loaderUrl: "Build/Build_WebGL.loader.js",
-    dataUrl: "Build/Build_WebGL.data",
-    frameworkUrl: "Build/Build_WebGL.framework.js",
-    codeUrl: "Build/Build_WebGL.wasm",
-  });
-
-  function handleClickEnterFullscreen() {
-    requestFullscreen(true);
-  }
-
-  const handleGameOver = useCallback((userName, score) => {
-    setUserName(userName);
-    setScore(score);
-  }, []);
-
-  useEffect(() => {
-    addEventListener("CallReact", handleGameOver);
-    return () => {
-      removeEventListener("CallReact", handleGameOver);
-    };
-  }, [addEventListener, removeEventListener, handleGameOver]);
-
-  function TestA() {
+  function gameManagerBtnClick() {
     sendMessage("GameManager", "BtnClick");
   }
 
   return (
     <div>
-      <PrimarySearchAppBar />
+      <PrimarySearchAppBar
+        gameManagerBtnClick={gameManagerBtnClick}
+        addEventListener={addEventListener}
+        removeEventListener={removeEventListener}
+      />
       <Unity
         unityProvider={unityProvider}
         style={{
@@ -52,7 +31,6 @@ function App() {
           alignSelf: "center",
         }}
       />
-      {/* {<h1>{`불럿다 저는 ${userName} 이고 점수는 ${score} 입니다.`}</h1>} */}
     </div>
   );
 }
